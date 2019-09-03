7ac8d9e30247ab8b11cae1ac08e1a51390c26ca9
Rich Sharp <richpsharp@gmail.com>
2019-09-02 21:46:19 -0700

    0 workers

--- ppl_travel_coverage.py
+++ ppl_travel_coverage.py
@@ -49,7 +49,7 @@ MAX_TRAVEL_DISTANCE = 20000
 # used to avoid computing paths where the population is too low
 POPULATION_COUNT_CUTOFF = 100
 
-TASKGRAPH_WORKERS = multiprocessing.cpu_count()
+TASKGRAPH_WORKERS = 0 # multiprocessing.cpu_count()
 
 logging.basicConfig(
     level=logging.DEBUG,
