c4e6bb7fed25972286e41a12a8658cbcc898043a
Rich Sharp <richpsharp@gmail.com>
2021-03-25 06:11:17 -0700

    debugging

--- ppl_travel_coverage.py
+++ ppl_travel_coverage.py
@@ -578,6 +578,7 @@ def access_raster_worker(
 
             (i_offset, j_offset, population_reach, norm_population_reach) = payload
             j_size, i_size = population_reach.shape
+            LOGGER.info(f'got payload for {i_offset} {j_offset} {i_size} {j_size}')
             current_pop_reach = people_access_band.ReadAsArray(
                 xoff=i_offset, yoff=j_offset,
                 win_xsize=i_size, win_ysize=j_size)
