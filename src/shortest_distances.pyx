3b3d5e5a3d251529622fd6a12e6346ab7e6e3afd
Rich Sharp <richpsharp@gmail.com>
2021-03-25 06:42:14 -0700

    getting the types to match up

--- src/shortest_distances.pyx
+++ src/shortest_distances.pyx
@@ -106,8 +106,8 @@ def find_population_reach(
         cell_length_m*2**0.5,
         cell_length_m,
         cell_length_m*2**0.5]
-    cdef float frict_n, c_time, n_time, population_val
-    cdef int i_start, j_start, i_n, j_n
+    cdef float frict_n, c_time, n_time
+    cdef int i_start, j_start, i_n, j_n, population_val
 
     cdef DistPriorityQueueType dist_queue
     cdef ValuePixelType pixel
