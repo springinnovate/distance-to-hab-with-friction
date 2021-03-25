bac30ecb7951a9705cbe279282d647ad2f9dccae
Rich Sharp <richpsharp@gmail.com>
2021-03-25 10:26:23 -0700

    run a script scenario

--- doit.sh
+++ doit.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 python setup.py install
-python ppl_travel_coverage.py $@
+python ppl_travel_coverage.py lspop_2017_URCA_rural 60 --countries germany france
+python ppl_travel_coverage.py lspop_2017_URCA_urban 60 --countries germany france
+python ppl_travel_coverage.py lspop_2017_URCA_rural 120 --countries germany france
+python ppl_travel_coverage.py lspop_2017_URCA_urban 120 --countries germany france
